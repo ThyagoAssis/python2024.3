968763704cf5377ed1d13143a8efe3ee18bd92c2
PROGRAMAÇÃO EM PYTHON 2024.3 <51129532024.3@senacrjedu.local>
2024-06-10 21:51:12 -0300

    Aulas 01

---
+++ Aula 01/desafio.py
@@ -0,0 +1,12 @@
+'''
+    Solicitar ao usuario a cor da camisa e salvar a resposata
+    dentro de uma variável
+
+    Ao final exiba a cor da camisa
+'''
+
+#A função input recebe uma informação que vem de fora
+cor_camisa = input("Qual a cor da sua camisa? ")
+
+#Saida da informação
+print("A cor da camisa é: ", cor_camisa);
---
+++ Aula 01/variaveis.py
@@ -0,0 +1,27 @@
+#Variavel é uma caixinha onde guardamos nossas cosias
+
+'''
+Principais tipos de variaveis no python
+    >string - Texto/Caracteres
+    >int - Valores inteiros (ex: 20)
+    >float - Numeros fracionários (ex: 3.9)
+    >booleano - Verdadeiro / Falso
+    >list - Criar uma lista de itens (ex: [1,2,3])
+    >tuplas - Semelhante a lista porem seu conteudo é imutavel
+              ex: (1,2,3)
+'''
+#Declarando as variaveis
+curso = "Senac"
+valor = 10
+valor_dois = 3.5
+verdade = False
+animais = ["Leao","Vaca","Cachorro"]
+numeros = (1,2,3)
+
+#Verificar o tipo de cada variavel
+#A função type exibe o tipo da variavel
+
+print("O tipo é: ", type(verdade))
+
+
+
