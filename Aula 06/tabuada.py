1555633ad8031c68f433d23be00cc9e5c39c4f4a
PROGRAMAÇÃO EM PYTHON 2024.3 <51129532024.3@senacrjedu.local>
2024-07-01 20:30:16 -0300

    Aula 06

--- Aula 06/tabuada.py
+++ Aula 06/tabuada.py
@@ -1,7 +1,7 @@
 def tabuada():
     num = int(input("Tabuada do numero?  "))
     op = input("Informe a aoperação: ")
-    for i in range(0,10+1):
+    for i in range(1,10+1):
       if op == "+":
           resultado = num + i
           print(f"{num} + {i} = {resultado}")
